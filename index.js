deec9a5963348c07161ef7e7df154e6935d7f6ff
Michael Boroski <mjboroski@gmail.com>
2018-07-07 21:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function distanceTravelledInFeet(start, destination){
 
 function calculatesFarePrice(start, destination){
   const distance = distanceTravelledInFeet(start, destination);
-  
+
     if (distance <= 400) {
       return 0;
     } else if (distance > 400 && distance <= 2000) {
-      return .02 * distance;
+      return .02 * (distance - 400);
     } else if (distance > 2000 && distance < 2500) {
       return 25;
     } else {
