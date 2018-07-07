d52dd259fe59a08dc63c5ea9c1c0c7e58f584ba3
Michael Boroski <mjboroski@gmail.com>
2018-07-07 21:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  return |street - 42|
+}
+
+function distanceFromHqInFeet(){
+
+}
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+}
